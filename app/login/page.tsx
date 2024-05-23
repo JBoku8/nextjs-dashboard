09fa28fb0489978f6784d453a5aa8b2e9372185b
Jano Bokuchava <m.prog@yahoo.com>
2024-05-23 23:11:12 +0400

    tutorial chapter 15-16 changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,14 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Sign In',
+};
+
+
+
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
